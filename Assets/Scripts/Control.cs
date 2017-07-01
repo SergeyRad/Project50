e9122cb3438cc1a04bec2061a8c4312a62d15193
ldevlnull <numberlnull@gmail.com>
2017-07-01 14:30:40 +0300

    fixed script auto-merge

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -4,47 +4,31 @@ using UnityEngine;
 
 public class Control : MonoBehaviour {
 
-    private bool is_bot = false;
-    private float vertical;
-    private float horizontal;
-    private Vector3 mouse_position;
-    private float x, y;
-    private Vector2 force_point;
-    private Rigidbody2D cache_rigidbody;
-    private Vector2 movement_check;
-<<<<<<< HEAD:Assets/script/Control.cs
-    private KeyCode key_shoot = KeyCode.Mouse0;
-    private Camera camera;
-=======
-    private Camera player_camera;
->>>>>>> 00000001:Assets/Scripts/Control.cs
-    private Texture2D[] numeral = new Texture2D[10];
-
     public int ammo = 30;
-    private bool isReload = false;
-    private int health = 100;
+    public bool in_menu;
+    public float force = 15;
+    public float shooting_speed;
 
+    public Canvas menu;
     public GameObject player;
     public GameObject bullet;
     public GameObject weapon;
-    public float shooting_speed;
-    public float force = 15;
-    public SpriteRenderer[] damage = new SpriteRenderer[3];
     public SpriteRenderer[] fire = new SpriteRenderer[3];
-    public Canvas menu;
-    public bool in_menu;
-<<<<<<< HEAD:Assets/script/Control.cs
-    
-    void Start()
-    {
-        cache_rigidbody = GetComponent<Rigidbody2D>();
-        camera = Camera.main;
-        camera.GetComponent<Cam>().enabled = true;
-        camera.GetComponent<Cam>().player = gameObject.transform;
-        menu = camera.GetComponent<Clientside>().menu;
-        camera.GetComponent<CursorController>().SetCursor();
-        this.numeral = camera.GetComponent<Clientside>().numeral;
-=======
+    public SpriteRenderer[] damage = new SpriteRenderer[3];
+
+    private int health = 100;
+    private bool is_bot = false;
+    private bool isReload = false;
+    private float x, y;
+    private float vertical;
+    private float horizontal;
+
+    private Camera player_camera;
+    private Vector2 force_point;
+    private Vector2 movement_check;
+    private Vector3 mouse_position;
+    private Texture2D[] numeral = new Texture2D[10];
+    private Rigidbody2D cache_rigidbody;
 
     void Start() {
         cache_rigidbody = GetComponent<Rigidbody2D>();
@@ -54,7 +38,6 @@ public class Control : MonoBehaviour {
         menu = player_camera.GetComponent<Clientside>().menu;
         player_camera.GetComponent<CursorController>().SetCursor();
         this.numeral = player_camera.GetComponent<Clientside>().numeral;
->>>>>>> 00000001:Assets/Scripts/Control.cs
     }
     void Shoot() {
         float posX = this.transform.position.x + (Mathf.Cos((transform.localEulerAngles.z - 90) * Mathf.Deg2Rad)) * -shooting_speed;
@@ -65,34 +48,17 @@ public class Control : MonoBehaviour {
     }
     void Update() {
         // In-game Menu
-        if (Input.GetKeyDown(Settings.keyMenu)) {
-<<<<<<< HEAD:Assets/script/Control.cs
-            if (!in_menu)
-            {
-                menu.enabled = in_menu = !in_menu;
-                camera.GetComponent<CursorController>().SetMenuCursor();
-            }
-            else
-            {
-                menu.enabled = in_menu = !in_menu;
-                camera.GetComponent<CursorController>().SetCursor();
-            }
-            
-=======
-            if (!in_menu) {
-                menu.enabled = in_menu = !in_menu;
-                player_camera.GetComponent<CursorController>().SetMenuCursor();
-            } else {
-                menu.enabled = in_menu = !in_menu;
-                player_camera.GetComponent<CursorController>().SetCursor();
-            }
-
->>>>>>> 00000001:Assets/Scripts/Control.cs
+        if(!in_menu) {
+            menu.enabled = in_menu = !in_menu;
+            player_camera.GetComponent<CursorController>().SetMenuCursor();
+        } else {
+            menu.enabled = in_menu = !in_menu;
+            player_camera.GetComponent<CursorController>().SetCursor();
         }
-        if (Input.GetKeyDown(Settings.keyShoot) && !in_menu) {
+        if(Input.GetKeyDown(Settings.keyShoot) && !in_menu) {
             this.SendAttack();
         }
-        if (Input.GetKeyDown(Settings.keyReload)) {
+        if(Input.GetKeyDown(Settings.keyReload)) {
             this.Reload();
         }
     }
@@ -100,63 +66,29 @@ public class Control : MonoBehaviour {
     void FixedUpdate() {
         movement_check.x = transform.position.x;
         movement_check.y = transform.position.y;
-        if (!is_bot) {
+        if(!is_bot) {
             // Movement
             mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             vertical = Input.GetAxis("Vertical");
             horizontal = Input.GetAxis("Horizontal");
-            var angle = Vector2.Angle(Vector2.right, mouse_position - transform.position); //угол между вектором от объекта к мыше и осью х
+            var angle = Vector2.Angle(Vector2.right, mouse_position - transform.position); // угол между вектором от объекта к мыше и осью х
                                                                                            // TODO: make something better, than Elvis operator
             transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < mouse_position.y ? angle - 90 : -angle - 90);
             cache_rigidbody.AddForce(new Vector2(force * horizontal, force * vertical));
         }
 
-<<<<<<< HEAD:Assets/script/Control.cs
-        if (isReload)
-        {
-            if (ammo < 30)
+        if(isReload) {
+            if(ammo < 30)
                 ammo++;
-            if (ammo >= 30)
-            {
-                this.Reload();
-            }
-        }
-    }
-
-    public void SendAttack()
-    {
-        if (ammo > 1)
-        {
-            if (!isReload)
-            {
-                ammo--;
-                camera.GetComponent<Clientside>().Attack();
-            }
-        }
-        else
-        {
-            this.Reload();
-        }
-    }
-
-    public void Reload()
-    {
-        isReload = !isReload;
-    }
-
-=======
-        if (isReload) {
-            if (ammo < 30)
-                ammo++;
-            if (ammo >= 30) {
+            if(ammo >= 30) {
                 this.Reload();
             }
         }
     }
 
     public void SendAttack() {
-        if (ammo > 1) {
-            if (!isReload) {
+        if(ammo > 1) {
+            if(!isReload) {
                 ammo--;
                 player_camera.GetComponent<Clientside>().Attack();
             }
@@ -168,70 +100,45 @@ public class Control : MonoBehaviour {
     public void Reload() {
         isReload = !isReload;
     }
-
->>>>>>> 00000001:Assets/Scripts/Control.cs
     public void SetHealth(int hp) {
         this.health = hp;
     }
 
-<<<<<<< HEAD:Assets/script/Control.cs
-    void OnTriggerEnter2D(Collider2D other)
-    {
-=======
     void OnTriggerEnter2D(Collider2D other) {
->>>>>>> 00000001:Assets/Scripts/Control.cs
         // Damage
-        if (other.gameObject.tag == "Bullet" && other.GetComponent<Bullet>().master != gameObject) {
+        if(other.gameObject.tag == "Bullet" && other.GetComponent<Bullet>().master != gameObject) {
             Destroy(other.gameObject);
             Camera.main.GetComponent<Clientside>().Hit(this.name);
-            if (!damage[0].enabled && !damage[1].enabled && !damage[2].enabled)
+            if(!damage[0].enabled && !damage[1].enabled && !damage[2].enabled)
                 StartCoroutine(Hit(0));
-            else if (damage[0].enabled && !damage[1].enabled && !damage[2].enabled)
+            else if(damage[0].enabled && !damage[1].enabled && !damage[2].enabled)
                 StartCoroutine(Hit(1));
-            else if (damage[0].enabled && damage[1].enabled && !damage[2].enabled)
+            else if(damage[0].enabled && damage[1].enabled && !damage[2].enabled)
                 StartCoroutine(Hit(2));
         }
     }
     IEnumerator Hit(int dmg) {
         damage[dmg].enabled = true;
         yield return new WaitForSeconds(2f);
-        if (damage[0].enabled && !damage[1].enabled && !damage[2].enabled)
+        if(damage[0].enabled && !damage[1].enabled && !damage[2].enabled)
             damage[0].enabled = false;
-        else if (damage[0].enabled && damage[1].enabled && !damage[2].enabled)
+        else if(damage[0].enabled && damage[1].enabled && !damage[2].enabled)
             damage[1].enabled = false;
-        else if (damage[0].enabled && damage[1].enabled && damage[2].enabled)
+        else if(damage[0].enabled && damage[1].enabled && damage[2].enabled)
             damage[2].enabled = false;
     }
 
-<<<<<<< HEAD:Assets/script/Control.cs
-    private void OnGUI()
-    {
-        var mouse = Input.mousePosition;
-        if (ammo > 9)
-        {
-=======
     private void OnGUI() {
         var mouse = Input.mousePosition;
-        if (ammo > 9) {
->>>>>>> 00000001:Assets/Scripts/Control.cs
+        if(ammo > 9) {
             var x = ammo % 10;
             var y = (int)ammo / 10;
             GUI.DrawTexture(new Rect(mouse.x + 18, -mouse.y + Screen.height - 8, 16, 16), numeral[y]);
             GUI.DrawTexture(new Rect(mouse.x + 35, -mouse.y + Screen.height - 8, 16, 16), numeral[x]);
-<<<<<<< HEAD:Assets/script/Control.cs
-        }
-        else
-        {
-            GUI.DrawTexture(new Rect(mouse.x + 18, -mouse.y + Screen.height - 8, 16, 16), numeral[0]);
-            GUI.DrawTexture(new Rect(mouse.x + 35, -mouse.y + Screen.height - 8, 16, 16), numeral[ammo]);
-        }
-        GUI.Label(new Rect(120f, 24f, 100, 21), health.ToString(), camera.GetComponent<Clientside>().style);
-=======
         } else {
             GUI.DrawTexture(new Rect(mouse.x + 18, -mouse.y + Screen.height - 8, 16, 16), numeral[0]);
             GUI.DrawTexture(new Rect(mouse.x + 35, -mouse.y + Screen.height - 8, 16, 16), numeral[ammo]);
         }
         GUI.Label(new Rect(120f, 24f, 100, 21), health.ToString(), player_camera.GetComponent<Clientside>().style);
->>>>>>> 00000001:Assets/Scripts/Control.cs
     }
 }
